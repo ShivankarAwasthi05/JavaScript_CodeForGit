c1e189fac3af6cf1db26eed6a81f32e52aa7ab50
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-02-13 04:48:59 +0000

    container commit done

---
+++ 01_Basics/datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //Treat all JS code as newer version
+
+// alert(3+3) // We are using nodejs, not browser
+
+console.log(3+3) // code readibilty should be maintain
+console.log("RadhaRani")
+
+let name="radha Rani"
+let age=18
+let isLoggedIn=false
+
+//Primitive Datatypes as mentioned below
+
+//number =>2 to power 53
+//bigint 
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => value does not known
+//symbol => unique
+
+// Objects
+
+console.log(typeof(age));
+console.log(typeof null);
+console.log(typeof undefined);
