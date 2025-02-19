a5bd298167f851ea347dfba52ddacff5ad21249d
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-02-19 08:17:51 +0000

    .

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -14,3 +14,18 @@ let typeNumber=Number(value)
 console.log(typeof typeNumber);
 console.log(typeNumber);//note a number(NAN) 
 console.log(typeof null);
+
+/*
+"33" => 33
+"33abc" => NaN
+//true =>1; false =>0
+*/
+
+let isloggedIn = "Radha Rani"
+let booleanIsLoggedIn=Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
