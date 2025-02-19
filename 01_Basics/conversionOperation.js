756a41c5f9dc744ddfdac08d0bdbcf45b48ac6c8
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-02-19 04:17:45 +0000

    container commit done

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -1,8 +1,17 @@
 let score=33
+
+// typeof operator is used to know what type of value variable is storing 
+
 console.log(typeof score);
 console.log(typeof(score));
+let name="Radha Rani"
 
 //conversion of datatype
 
+console.log(typeof name);
 let valueNumber=String(score);
 console.log(typeof valueNumber);
+let name2=Number(name);
+console.log(typeof name2);
+
+
