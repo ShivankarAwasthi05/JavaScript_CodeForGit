70450dbb1267a1cda926cc34dfeb64055918bca3
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-02-20 04:20:00 +0000

    .

---
+++ 01_Basics/operation.js
@@ -0,0 +1,45 @@
+// Operations in JavaScript 
+//****************Operations********************************* */
+
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+
+//Arithmetic Operation
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+
+let str1="Radha"
+let str2="Rani"
+console.log(str1+str2);
+
+console.log("1" +2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+//console.log(true+);  //this is node a good way to define a expression
+
+console.log(+"");
+console.log((3+4) * 5 /3); // Make sure to use parenthesis for this type of operation to be perform 
+
+/* let num1,num2,num3
+num1=num2=num3=2+2      // Make sure your code is perfectly readable for the other user as well
+console.log(num1);
+*/
+
+/******************************************Prefix and Postfix operation**************************************** */
+let score = 12;
+console.log(score++); // postfix operation variable ++/--;
+
+let score1=12;
+console.log(++score1); // prefix operation ++/-- variable;
+
