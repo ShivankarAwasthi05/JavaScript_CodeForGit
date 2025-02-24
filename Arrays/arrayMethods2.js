57dfa0cca661ecef0d157cef701d5fe1914a8e1d
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-02-24 16:39:44 +0000

    container commit done

---
+++ Arrays/arrayMethods2.js
@@ -0,0 +1,9 @@
+const godess=["RadhaRani","Kishoriji","ShyamaShyam","Radhikaji"];
+const godess2=["Ramya","Radha","Shriji","Vrindawaneshwari"];
+//godess.push(godess2);
+//console.log(godess);
+const godess3=godess.concat(godess2);
+console.log(godess3);
+
+const godess4=[...godess,...godess2];
+console.log(godess4);
