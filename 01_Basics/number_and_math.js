bee2156d0b90522354c5aa4f6eb43683fcb0600f
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-02-24 04:25:28 +0000

    container commit done

---
+++ 01_Basics/number_and_math.js
@@ -0,0 +1,21 @@
+const score=108
+console.log(score);
+const num=new Number(108)
+console.log(num);
+console.log(num.toString());
+console.log(num.toFixed(2));
+
+const num2=23.897
+console.log(num2.toLocaleString());
+console.log(num2.toPrecision(3));
+
+const rupees=1000000
+console.log(rupees.toLocaleString('en-IN'));
+
+/***************************************************Maths**************************************************************** */
+
+console.log(Math);
+console.log(Math.round(-4));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+console.log(Math.sqrt(36));
---
+++ 01_Basics/stringFunctions.js
@@ -0,0 +1,18 @@
+const str=new String("RadhaRani");
+console.log(str);
+console.log(str.toUpperCase());
+const str1=new String("Kishoriji");
+const str2=new String("Shyamaji");
+console.log(`The Divine name of godess are ${str} and ${str2} and ${str1}`);
+console.log(str.toLowerCase());
+console.log(str.concat(str1));
+const name="ShriShyamaShyam-ShriRadhaRani-ShriRadhika";
+console.log(name.replaceAll('-',' '));
+const name2=new String("Shri Radha Rani");
+console.log(name2.toLocaleUpperCase('en-IN'));
+console.log(name2.toLocaleLowerCase('en-IN'));
+console.log(str.length);
+const name3=new String("ShriRadhaRani");
+console.log(name3.substring(2,13));
+console.log(name3.slice(-13,7));
+
