fe6909986da21102af8e6d2ccce574513ff7e7db
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-02-27 03:53:42 +0000

    container config done

---
+++ JSON/json.js
@@ -0,0 +1,20 @@
+console.log("RadhaVallabhShrihariVansh");
+// {
+//     "name":"RadhaVallabhShriHariVansh",
+//     "name1":"Shriji",
+//     "name2":"ShriKrishna",
+// }
+
+//This above mentioned is also an example of json format in javascript how the data is coming from the database.
+
+//JSON coming as the fromat of the array(object):Means arrays inside contains objects as the data from the database.
+
+/*Example:
+[
+{},
+{},
+{},
+{}
+]
+This also a type of json in javascript how the data is coming from the data base.
+*/
