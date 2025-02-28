6151f91548793fa0d673a7cf726a66c90697c7a3
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-02-28 10:29:05 +0000

    container config done

--- Function/arrow.js
+++ Function/arrow.js
@@ -57,5 +57,30 @@ console.log(funExplicit("ShriRadha","Rani"));
 
 //Implicit Arrow Function
 
-const funImplicit(name1,name2) => name1+name2;
+const funImplicit=(name1,name2) => name1+name2;
 console.log(funImplicit("ShriShyama","Shyam"))
+
+/* Note: If we using implicit arrow function with {} then we have to use return keyword otherwise if we using () brackets the we don't have to use return keyword and if we don't want to use any bracket then we can do this also
+*/
+
+const addTwoNumber = (num1,num2) =>({Username:"Shriji"});
+console.log(addTwoNumber(2,3));
+
+/* If we Take case of returning object using implict arrow function then we have to use the {} brackets because it is necesary in case of object return 
+*/
+
+
+// const ReFun= function(name){
+//     console.log(`The Ultimate name of godess ${name}`);
+//     console.log(this);
+// }
+// ReFun("Shriji");
+
+// const ReFunn =(name1) =>("Shriji");
+// const namee=ReFunn();
+// console.log(namee);
+
+// const Refunnn=(nameee) =>{
+//     console.log(this);
+// }
+// Refunnn();
