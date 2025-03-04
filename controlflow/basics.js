5492a174f3f63a70a006a6f799823cbe7279fb08
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-03-04 05:05:13 +0000

    container config done

---
+++ controlflow/basics.js
@@ -0,0 +1,34 @@
+// control Statements in javascript 
+ /*
+1)if Statments 
+Syntax:
+        if(condition){
+        Statements;
+        }
+ */
+/*
+        if(true){
+            console.log("ShriRadhaVallabhShriHariVansh");
+        }
+        
+*/
+
+//Basics Operators in Javscript:(<)less than,(>)Greater than,(>=)less tha or equals to or (<=) greater than or equals to, (==)equals,(!=)not equals, (===)type equals to,(!==)
+
+console.log(2==="2"); //Strict equals False;
+console.log(-4!==-4);
+console.log(-4!==4);
+
+const temperature=200;
+if(temperature>100){
+    const temp=200;
+    console.log(`temperature is High as ${temp}`);
+}
+console.log("RadhVallabhShriHariVansh");
+
+if(temperature>300){
+    console.log("Temperature is greater than 300");
+}
+else{
+console.log("temperature is less than 300");
+}
