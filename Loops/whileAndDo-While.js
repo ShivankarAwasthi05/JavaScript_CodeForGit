54b2c30773a3dbd975e17792ee1fd934cdf32fcd
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-03-05 07:00:11 +0000

    container commit done

---
+++ Loops/whileAndDo-While.js
@@ -0,0 +1,10 @@
+//while and do-while loop in javascript
+let i=0;
+while(i<=10){
+    console.log(`Value of Index is ${i}`);
+    i=i+2;
+}
+do(i<=10){
+    console.log(`Value of Index is ${i}`);
+    i=i+2;
+}
