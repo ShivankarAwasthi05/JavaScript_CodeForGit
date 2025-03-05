35bb949372630b91e2e0080c613b6102d0da2e78
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-03-05 08:26:17 +0000

    container config done

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,11 @@
-console.log("Shri Radha Rani")
+console.log("Shri Radha Rani");
+const syn=Symbol("Key");
+const obj={
+    Name:"Aditya Awasthi",
+    Roll_Number:2201641520164,
+    Branch:"CS_AI",
+    ID:32710
+    [syn]="key"
+}
+console.log(obj["ID"]);
+console.log(obj[syn]);
