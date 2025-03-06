073e79d665b4c0ad480a605767bbf8778990d944
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-03-06 05:33:04 +0000

    container config done

---
+++ Loops/MoreOnLoops3.js
@@ -0,0 +1,23 @@
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+// const userBooks=books.filter((num) =>num.genre=="History");
+// console.log(userBooks);
+
+// const userBooks2=books.filter((num) =>num.publish >= 2000);
+// console.log(userBooks2);
+
+// const userBooks3=books.filter((num) =>num.publish >= 1995 && num.genre==="Science");
+// console.log(userBooks3);
+
+// const userBooks4=books.filter((num) =>num.publish >= 1995 && num.genre==="Science" && num.publish >=2010);
+// console.log(userBooks4);
