d88597951d03b60f35e4e13a20f79085b6388a5f
shivankarAwasthi <152833921+shivankarAwasthi@users.noreply.github.com>
2025-03-06 04:02:55 +0000

    container config done

--- Loops/whileAndDo-While.js
+++ Loops/whileAndDo-While.js
@@ -4,7 +4,19 @@ while(i<=10){
     console.log(`Value of Index is ${i}`);
     i=i+2;
 }
-do(i<=10){
-    console.log(`Value of Index is ${i}`);
-    i=i+2;
+
+let myArr=["RadhaRani","Radhika","Shriji","ShyamaShyam"];
+let arr=0;
+while(arr<=myArr.length-1){
+    console.log(myArr[arr]);
+    arr++;
 }
+
+//do-while Loop in javascript
+
+let score=11;
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score<=10);
+
